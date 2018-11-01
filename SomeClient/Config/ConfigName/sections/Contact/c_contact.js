d1322c40bcc8a612625d94714c66656167e53feb
miv <mivxxx@gmail.com>
2018-11-01 11:13:32 +0300

    Add event handler on matrix field example

--- SomeClient/Config/ConfigName/sections/Contact/c_contact.js
+++ SomeClient/Config/ConfigName/sections/Contact/c_contact.js
@@ -3,6 +3,14 @@
  */
 
 irisControllers.classes.c_Contact_custom = irisControllers.classes.c_Contact.extend({
+  events: {
+    'field:edited #ContactDateTypeID__d_Contact_Date_Matrix': 'onChangeHandler',
+  },
+
+  onChangeHandler: function () {
+    console.log('matrix field changed');
+  },
+
   onOpen: function() {
     // Родительский обработчик
     irisControllers.classes.c_Contact_custom.__super__.onOpen.call(this);
